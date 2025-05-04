8dc4d858a41955f569e969129299333eaf4e2c12
Kajetan Respondek <kajetanrespondek@gmail.com>
2025-05-04 19:21:30 +0200

    first logs implementation

--- skrypt.sh
+++ skrypt.sh
@@ -10,5 +10,18 @@ fi
 if [ "$Argument" == "--date" ]
 then
     echo "Data i czas: $(date)"
-    exit 1
-fi
+    exit 0
+elif [ "$Argument" == "logs" ]
+then
+    echo "Logs"
+    if [ -z "$2" ]
+    then
+        Range=100
+    else
+        Range=$2
+    fi
+
+    for i in $(seq 1 $Range)
+    do
+        echo "Log $i, skrypt.sh, data: $(date)" >> "log{$i}.txt"
+    done
